d73e3be4937b8ba25a9abf556d9c3c2c7d02f076
Choco <koskela.nelson@gmail.com>
2020-09-15 20:22:11 -0700

    Switching computers

--- app/App.py
+++ app/App.py
@@ -2,10 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/<name>')
+@app.route('/name')
 def test(name):
-	return '<h1> Hello World {}!</h1>'.format(name)
+	# return '<h1> Hello World {}!</h1>'.format(name)
+	return '<h1>	Hello Nelson</h1>'
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/courses')
+def course():
+	return render_template('courses.html')
+
+@app.route('/googlecalender')
+def GoogleCalender():
+	return render_template('GoogleCalander.html')
